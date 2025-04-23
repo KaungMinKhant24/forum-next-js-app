b41be2a8956e8dd16fbdce80e962c8d5ee780238
KaungMinKhant <kaungminkhant.edka@gmail.com>
2025-04-23 22:13:23 +0630

    design layout

---
+++ components/NavBar.tsx
@@ -0,0 +1,43 @@
+import Image from "next/image";
+import React from "react";
+
+import logo from "../public/logo.jpg";
+import profile from "../public/profile.jpg";
+
+function Navbar() {
+  return (
+    <nav className="flex justify-between px-10 py-6">
+      <div className="flex items-center justify-center space-x-4">
+        <Image
+          src={logo}
+          width={60}
+          height={60}
+          className="rounded-full"
+          alt="logo"
+        />
+        <h1 className="font-bold">
+          Creative <span className="text-main">Coder</span>
+        </h1>
+      </div>
+      <div>
+        <input
+          type="text"
+          className="w-[600px] rounded-md bg-primary px-4 py-2"
+          placeholder="search anything globally"
+        />
+      </div>
+
+      <div>
+        <Image
+          src={profile}
+          width={45}
+          height={45}
+          className="aspect-square rounded-full object-cover"
+          alt="logo"
+        />
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
